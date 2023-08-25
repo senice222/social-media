0a0021b04e8737b212a198709b0e793b0c814349
Artem <sasdasd920@gmail.com>
2023-08-25 14:29:13 +0300

    initialized react-vite client config and structure

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import style from './App.module.scss'
+
+function App() {
+
+    return (
+        <>
+            <p className={style.number}>123</p>
+        </>
+    )
+}
+
+export default App
