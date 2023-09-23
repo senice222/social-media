632cdd4173a144c3c8ea4cf3c9ddee008c61652e
Artem <sasdasd920@gmail.com>
2023-09-23 11:53:48 +0300

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         react(),
         VitePluginFonts({
             google: {
-                families: ['Poppins'],
+                families: ['Poppins', 'Inter'],
             },
         }),
     ],
