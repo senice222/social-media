ce55b55fff7cfb7fb533f3f006457fbb803971eb
Artem <sasdasd920@gmail.com>
2023-09-25 21:31:31 +0300

    refactor

--- client/src/components/Comments/CommentsList.tsx
+++ client/src/components/Comments/CommentsList.tsx
@@ -1,30 +1,17 @@
 import style from './Comment.module.scss'
 import {FC, useState} from "react";
 import {Comment, CommentProps} from "../../interfaces/PostsI.ts";
-import * as Api from '../../api'
-import useSWR from 'swr';
-import {fetcher} from "../../core/axios.ts";
 import CommentsPagination from "../CommentsPagination/CommentsPagination.tsx";
 import CommentItem from "./CommentItem.tsx";
 import CreateComment from "../CreateComment/CreateComment.tsx";
+import {usePostComments} from "../../hooks/usePostComments.ts";
 
 const CommentsList: FC<CommentProps> = ({_id}) => {
     const [comment, setComment] = useState<string>('')
-    const {data: comments, mutate: mutateComments} = useSWR(`posts/comments/${_id}`, fetcher)
     const [currentPage, setCurrentPage] = useState<number>(1)
+    const {comments, handleComment} = usePostComments(_id, comment, setComment)
+
     const commentsPerPage = 5
-
-    const onCreateComment = async () => {
-        try {
-            await Api.posts.createComment(_id, comment)
-            mutateComments()
-            setComment('')
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    // Calculate the index of the first and last comment on the current page (pagination logic)
     const indexOfLastComment = currentPage * commentsPerPage
     const indexOfFirstComment = indexOfLastComment - commentsPerPage
     const currentComments = comments?.slice(indexOfFirstComment, indexOfLastComment)
@@ -33,7 +20,7 @@ const CommentsList: FC<CommentProps> = ({_id}) => {
     return (
         <div className={style.commentContainer}>
             <CreateComment
-                onCreateComment={onCreateComment}
+                onCreateComment={handleComment}
                 comment={comment}
                 setComment={setComment}
             />
---
+++ client/src/hooks/usePostComments.ts
@@ -0,0 +1,20 @@
+import useSWR from 'swr';
+import * as Api from "../api/index.ts";
+import {fetcher} from "../core/axios.ts";
+import {SetStateAction, Dispatch} from "react";
+
+export const usePostComments = (_id: string, comment: string, setComment: Dispatch<SetStateAction<string>>) => {
+    const {data: comments, mutate: mutateComments} = useSWR(`posts/comments/${_id}`, fetcher)
+
+    const handleComment = async () => {
+        try {
+            await Api.posts.createComment(_id, comment)
+            mutateComments()
+            setComment('')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    return {comments, handleComment}
+}
